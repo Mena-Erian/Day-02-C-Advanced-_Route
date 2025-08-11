7bfd0f92750050e4d905fb2eee976fd814a2cb0e
Mena Erian <mena3koko2karas1@gmail.com>
2025-08-11 15:44:54 +0300

    create assingment project

---
+++ Assignment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+
+namespace Assignment
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            #region 1
+
+            int N = Helper.GetIntFromUserUsingTryParse("the Number of Size `N`:");
+
+            List<int> numbers = new List<int>(N);
+            for (int i = 0; i < N; i++)
+            {
+
+            }
+
+
+            #endregion
+        }
+    }
+}
